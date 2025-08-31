6da7ab56b7fa3efa1db2be81facb2cdbc896b6e3
NIRAJ KUMAR <128396343+Neerajhac96@users.noreply.github.com>
2025-08-31 03:42:08 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=14433
+let accountEmail="nirak@getComputedStyle.com"
+var accountPassword="1234"
+accountCity="delhi"
+
+// accountId=2
+/*
+prefer not to  use var
+because of issue in block in block scope and functional scope
+*/
+accountEmail="hc@hc.com"
+accountPassword="212121"
+accountCity="pune"
+
+console.table([accountEmail,accountPassword,accountCity])
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+" use strict";//treat all js code as newr version
+
+// alert("3+3"); //we are using nodejs , not browser
+
+console.log(3 + 3);
+
+let age=18
+
+
+//number => 2 to power 53
+//bigint
+//stribg=>""
+//boolean =>true/false
+//null =>standalone value
+//undefine =>
+    //symbol => unique
+
+
+//object 
+
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
